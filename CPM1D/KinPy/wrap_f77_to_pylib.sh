cc687449040f23296d1feaa3ee1a2a6aa3921559
ndarmage <daniele.tomatis@outlook.com>
2020-07-10 19:23:00 +0200

    restore kinpy wrapper

--- CPM1D/KinPy/wrap_f77_to_pylib.sh
+++ CPM1D/KinPy/wrap_f77_to_pylib.sh
@@ -3,11 +3,15 @@ FOPTS='-ffixed-line-length-72 -Wextra -Wall -std=legacy'
 ONLYS='dbskin bskin'
 
 # Daniele uses gfortran from gcc 8.3.0
-GCCPATH=/data/tmplca/dtomatis/tools/gcc/gcc-8.3.0
-F77=$GCCPATH/bin/gfortran
-INC=$GCCPATH/lib64
+# The following is system-dependent, change it accordingly,
+# or simply use your default compilers
+# GCCPATH=/data/tmplca/dtomatis/tools/gcc/gcc-8.3.0
+# F77=$GCCPATH/bin/gfortran
+F77=gfortran
+# INC=$GCCPATH/lib64
 
 unset LD_LIBRARY_FLAGS   # just in case was set before
-export LDFLAGS=-Wl,-rpath=$INC
+# uncomment the next line if INC was set
+# export LDFLAGS=-Wl,-rpath=$INC
 export NPY_DISTUTILS_APPEND_FLAGS=1
 python3 -m numpy.f2py -c --f77flags="$FOPTS" --f90flags="$FOPTS" --f77exec=$F77 --f90exec=$F77 algo609.f only: $ONLYS : -m algo609
